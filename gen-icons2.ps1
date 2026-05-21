859c2df878525d7ff45d9ea697f03b2d38d200bc
open-hearted <49687225+open-hearted@users.noreply.github.com>
2026-05-22 01:04:00 +0900

    feat: update PWA icons and manifests for improved mobile support

--- gen-icons2.ps1
+++ gen-icons2.ps1
@@ -1,27 +1,41 @@
-﻿
 Add-Type -AssemblyName System.Drawing
 
-$path1 = Join-Path $PWD "icon-wake.png"
-$bmp1 = New-Object System.Drawing.Bitmap(512, 512)
-$g1 = [System.Drawing.Graphics]::FromImage($bmp1)
-$b1 = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::DarkOrange)
-$g1.FillRectangle($b1, 0, 0, 512, 512)
-$bmp1.Save($path1, [System.Drawing.Imaging.ImageFormat]::Png)
-$g1.Dispose(); $bmp1.Dispose()
+$outputDir = if ($PSScriptRoot) { $PSScriptRoot } else { $PWD.Path }
 
-$path2 = Join-Path $PWD "icon-awake.png"
-$bmp2 = New-Object System.Drawing.Bitmap(512, 512)
-$g2 = [System.Drawing.Graphics]::FromImage($bmp2)
-$b2 = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::MediumSeaGreen)
-$g2.FillRectangle($b2, 0, 0, 512, 512)
-$bmp2.Save($path2, [System.Drawing.Imaging.ImageFormat]::Png)
-$g2.Dispose(); $bmp2.Dispose()
+function New-SolidIcon {
+    param(
+        [string]$Name,
+        [int]$Size,
+        [System.Drawing.Color]$Color
+    )
 
-$path3 = Join-Path $PWD "icon-sleep.png"
-$bmp3 = New-Object System.Drawing.Bitmap(512, 512)
-$g3 = [System.Drawing.Graphics]::FromImage($bmp3)
-$b3 = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::MidnightBlue)
-$g3.FillRectangle($b3, 0, 0, 512, 512)
-$bmp3.Save($path3, [System.Drawing.Imaging.ImageFormat]::Png)
-$g3.Dispose(); $bmp3.Dispose()
+    $path = Join-Path $outputDir $Name
+    $bmp = New-Object System.Drawing.Bitmap($Size, $Size)
+    $graphics = [System.Drawing.Graphics]::FromImage($bmp)
+    $brush = New-Object System.Drawing.SolidBrush($Color)
 
+    $graphics.FillRectangle($brush, 0, 0, $Size, $Size)
+    $bmp.Save($path, [System.Drawing.Imaging.ImageFormat]::Png)
+
+    $brush.Dispose()
+    $graphics.Dispose()
+    $bmp.Dispose()
+    Write-Output "Created $Name"
+}
+
+$icons = @(
+    @{ Prefix = "wake"; Color = [System.Drawing.ColorTranslator]::FromHtml("#ff8c00") },
+    @{ Prefix = "awake"; Color = [System.Drawing.ColorTranslator]::FromHtml("#3cb371") },
+    @{ Prefix = "sleep"; Color = [System.Drawing.ColorTranslator]::FromHtml("#191970") }
+)
+
+foreach ($icon in $icons) {
+    New-SolidIcon "icon-$($icon.Prefix).png" 512 $icon.Color
+    New-SolidIcon "icon-$($icon.Prefix)-192.png" 192 $icon.Color
+    New-SolidIcon "icon-$($icon.Prefix)-180.png" 180 $icon.Color
+}
+
+$quickLogColor = [System.Drawing.ColorTranslator]::FromHtml("#111111")
+New-SolidIcon "icon-512.png" 512 $quickLogColor
+New-SolidIcon "icon-192.png" 192 $quickLogColor
+New-SolidIcon "icon-180.png" 180 $quickLogColor
